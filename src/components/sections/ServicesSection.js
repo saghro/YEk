6cf301b62fee45fed29bd36e42f63e727d383c84
hamza <hmzaitahmad@gmail.com>
2024-06-02 11:05:16 +0100

    rmv fct

--- src/components/sections/ServicesSection.js
+++ src/components/sections/ServicesSection.js
@@ -53,14 +53,3 @@ function ServiceItem({ icon, title, description }) {
         </div>
     );
 }
-function ServiceItems({ icon, title, description }) {
-    return (
-        <div className="flex items-start gap-4">
-            {icon}
-            <div>
-                <h3 className="text-lg mb-2 font-bold">{title}</h3>
-                <p className="text-white-600 dark:text-white-400">{description}</p>
-            </div>
-        </div>
-    );
-}
